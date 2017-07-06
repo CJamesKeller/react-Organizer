efc14bb621fb11abc5d2acb05d7b46ef42699f9b
Chris Keller <cjameskeller@gmail.com>
2017-07-06 12:57:36 -0500

    modifying

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
+# octahedron
+
+## About
+
+For many years, I have used a set of eight facets to help organize my life, as
+well as to develop new ideas. I wanted to make this an app, to facilitate
+continued use of this system digitally.
+
+## Setup
+
+Just run `npm install`, and then `npm start`!
+
+## Built With
+
+* [create-react-app](https://github.com/facebookincubator/create-react-app)
--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import logo from './octahedron.png';
 import './App.css';
 
-class App extends Component {
-  render() {
+const App = () => {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-main">
+          <h2>Welcome to your octahedron</h2>
+        </div>
+        <div>
+          <p className="App-intro">
+            To get started, begin adding notes or tasks to your facets.
+          </p>
+      </div>
       </div>
     );
-  }
 }
 
 export default App;
