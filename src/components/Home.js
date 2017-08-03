d961e835cb96a4ee3c940138775c33799034ec07
Chris Keller <cjameskeller@gmail.com>
2017-08-03 15:25:45 -0500

    brought in another component

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import { Card, Col, Icon, Row } from "react-materialize";
 
 import LoginPersonForm from "./LoginPersonForm";
 import NewPersonForm from "./NewPersonForm";
+import Tetrahedron from "./Tetrahedron";
 
 const Home = () => {
     return (
@@ -10,15 +11,33 @@ const Home = () => {
             <Row>
                 <Col s={2} />
                 <Col s={8}>
-                    <Card title="Welcome">
-                        Intro to the site will go here.
-                    </Card>
-                    <LoginPersonForm />
-                    <Icon large>brightness_low</Icon>
-                    <NewPersonForm />
-                    <Card title="More info">
-                        Official looking info on the bottom.
-                    </Card>
+                    <Row>
+                        <Card title="Welcome">
+                            Intro to the site will go here.
+                        </Card>
+                    </Row>
+                    <Row>
+                        <LoginPersonForm />
+                    </Row>
+                    <Row>
+                        <Col s={2}></Col>
+                        <Col s={8} className="centralCol">
+                            <Icon large>brightness_low</Icon>
+                            <hr />
+                            <Tetrahedron />
+                            <hr />
+                            <Icon large>brightness_low</Icon>
+                        </Col>
+                        <Col s={2}></Col>
+                    </Row>
+                    <Row>
+                        <NewPersonForm />
+                    </Row>
+                    <Row>
+                        <Card title="More info">
+                            Official looking info on the bottom.
+                        </Card>
+                    </Row>
                 </Col>
                 <Col s={2} />
             </Row>
--- src/components/Tetrahedron.js
+++ src/components/Tetrahedron.js
@@ -3,9 +3,11 @@ import { Link } from "react-router-dom";
 
 const Tetrahedron = () => {
     return (
-        <div>
-            <p>An "interactive" [not] image will go here.</p>
-            <Link to="/facet/" + { props.facetName } >Link to facet</Link>
+        <div className="Tetrahedron">
+            <p>An "interactive" image will go here.</p>
+            <p>For now, a link to Google:</p>
+            <Link to="https://www.google.com">Google</Link>
+            {/* <Link to="/facet/" + { props.facetName } >Link to facet</Link> */}
         </div>
     );
 };
